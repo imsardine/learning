07f91b64191b840e02dc765338722d1c77e98577
Jeremy Kao <imsardine@gmail.com>
2016-07-05 18:19:14 +0800

    Provide default implmenetations to protocol requirements.

---
+++ swift/ProtocolExtensions.swift
@@ -0,0 +1,31 @@
+protocol Vehicle {
+    var description: String { get }
+    var numberOfWheels: Int { get }
+    func accelerate()
+}
+
+extension Vehicle {
+    var description: String {
+        return "A vehicle with \(numberOfWheels) wheel(s)."
+    }
+}
+
+class Bike: Vehicle {
+    let numberOfWheels = 2
+
+    func accelerate() {
+    }
+}
+
+class Car: Vehicle {
+    let numberOfWheels = 4
+
+    func accelerate() {
+    }
+}
+
+let bike = Bike(), car = Car()
+
+print("Bike: \(bike.description)")
+print("Car: \(car.description)")
+
