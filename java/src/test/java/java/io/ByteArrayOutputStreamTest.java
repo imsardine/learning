560e8cd93ebb923f01b474cc7ecb8d34c5d195ed
Jeremy Kao <imsardine@gmail.com>
2017-03-18 00:33:18 +0800

    [java] Update usage of ByteArrayOutputStream.

--- java/src/test/java/java/io/ByteArrayOutputStreamTest.java
+++ java/src/test/java/java/io/ByteArrayOutputStreamTest.java
@@ -14,7 +14,7 @@ public class ByteArrayOutputStreamTest {
     }
 
     @Test
-    public void write() throws Exception {
+    public void writeAllBytes() throws Exception {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         byte[] bytes = {97, 98, 99}; // abc
         out.write(bytes);
@@ -22,6 +22,28 @@ public class ByteArrayOutputStreamTest {
         assertEquals(3, out.size());
         assertArrayEquals(bytes, out.toByteArray());
         assertEquals("abc", out.toString());
+    }
+
+    @Test
+    public void writeSingleByte() throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(97);
+        out.write(98);
+        out.write(99);
+
+        assertEquals(3, out.size());
+        assertEquals("abc", out.toString());
+    }
+
+    @Test
+    public void writePartialBytes() throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] bytes = {97, 98, 99, 100, 101, 102, 103}; // a b c d e f g
+        out.write(bytes, 3, 3);
+
+        byte[] expected = {100, 101, 102};
+        assertArrayEquals(expected, out.toByteArray());
+        assertEquals("def", out.toString());
     }
 
     @Test
