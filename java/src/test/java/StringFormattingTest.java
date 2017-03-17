465a0b6f0f707daa9102fa3e5f06ec2e9c0e99ef
Jeremy Kao <imsardine@gmail.com>
2017-03-17 17:47:31 +0800

    [java] `System.setOut(null)` or `System.setErr(null)` will cause NPE.

--- java/src/test/java/StringFormattingTest.java
+++ java/src/test/java/StringFormattingTest.java
@@ -11,16 +11,25 @@ public class StringFormattingTest {
 
     private ByteArrayOutputStream err = new ByteArrayOutputStream();
 
+    private PrintStream systemOut;
+
+    private PrintStream systemErr;
+
     @Before
     public void repalceSystemOutAndErr() {
+        // Backup
+        systemOut = System.out;
+        systemErr = System.err;
+
         System.setOut(new PrintStream(out));
         System.setErr(new PrintStream(err));
     }
 
     @After
     public void resetSystemOutAndErr() {
-        System.setOut(null);
-        System.setErr(null);
+        // Restore. `System.setOut(null)` or `System.setErr(null)` will cause NPE.
+        System.setOut(systemOut);
+        System.setErr(systemErr);
     }
 
     @Test
