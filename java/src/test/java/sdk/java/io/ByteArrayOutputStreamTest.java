c709484f8b6420af23264a98acf090f82a142f5a
Jeremy Kao <imsardine@gmail.com>
2017-03-18 11:56:03 +0800

    [java] write(int) and behavior of ByteArrayOutputStream.

--- java/src/test/java/sdk/java/io/ByteArrayOutputStreamTest.java
+++ java/src/test/java/sdk/java/io/ByteArrayOutputStreamTest.java
@@ -16,14 +16,15 @@ public class ByteArrayOutputStreamTest {
     }
 
     @Test
-    public void writeAllBytes() throws Exception {
+    public void only8LowOrderBitsAreWriteen() {
+        // The byte to be written is the eight low-order bits of the argument b.
+        // The 24 high-order bits of b are ignored.
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        byte[] bytes = {97, 98, 99}; // abc
-        out.write(bytes);
+        int input = 0b11111111111111111111111101100001; // 'a' = 01100001 (97)
+        out.write(input);
 
-        assertEquals(3, out.size());
-        assertArrayEquals(bytes, out.toByteArray());
-        assertEquals("abc", out.toString());
+        assertEquals(1, out.size());
+        assertEquals(out.toByteArray()[0], 0b01100001);
     }
 
     @Test
@@ -38,14 +39,40 @@ public class ByteArrayOutputStreamTest {
     }
 
     @Test
+    public void writeAllBytes() throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] bytes = {97, 98, 99}; // abc
+        out.write(bytes);
+
+        assertEquals(3, out.size());
+        assertArrayEquals(bytes, out.toByteArray());
+        assertEquals("abc", out.toString());
+    }
+
+    @Test
     public void writePartialBytes() throws Exception {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         byte[] bytes = {97, 98, 99, 100, 101, 102, 103}; // a b c d e f g
         out.write(bytes, 3, 3);
 
-        byte[] expected = {100, 101, 102};
+        byte[] expected = {100, 101, 102}; // bytes[off] ~ bytes[off + len - 1]
         assertArrayEquals(expected, out.toByteArray());
         assertEquals("def", out.toString());
+    }
+
+    @Test(expected = Test.None.class)
+    public void close_SubsequentOperationsWontThrowExceptions() throws Exception {
+        // The methods in this class can be called after the stream has been
+        // closed without generating an IOException.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] bytes = {97, 98, 99}; // abc
+        out.write(bytes);
+
+        out.close();
+        assertEquals(3, out.size());
+
+        out.write(bytes);
+        assertEquals(6, out.size());
     }
 
     @Test
