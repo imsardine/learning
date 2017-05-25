a381a2f7a3606f0f9b7399ea1854f01ec5f41ec4
Jeremy Kao <imsardine@gmail.com>
2017-05-25 22:34:53 +0800

    [espresso] Add a recycler view

--- espresso/app/src/main/java/learning/espresso/MainActivity.java
+++ espresso/app/src/main/java/learning/espresso/MainActivity.java
@@ -1,7 +1,10 @@
 package learning.espresso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showRecyclerView(View view) {
+        startActivity(new Intent(this, RecyclerViewActivity.class));
+    }
+
 }
---
+++ espresso/app/src/main/java/learning/espresso/RecyclerViewActivity.java
@@ -0,0 +1,66 @@
+package learning.espresso;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setAdapter(new CustomAdapter());
+    }
+
+    class CustomAdapter extends Adapter<CustomViewHolder> {
+        private LayoutInflater inflater;
+        private List<String> dataItems;
+
+        CustomAdapter() {
+            inflater = LayoutInflater.from(RecyclerViewActivity.this);
+            dataItems = new ArrayList<>();
+            for (int i = 0; i <= 50; i++) {
+                dataItems.add("Item " + (i + 1));
+            }
+        }
+
+        @Override
+        public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = inflater.inflate(R.layout.list_item_layout, parent, false);
+            return new CustomViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(CustomViewHolder holder, int position) {
+            holder.textView.setText(dataItems.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataItems.size();
+        }
+    }
+
+    class CustomViewHolder extends ViewHolder {
+        TextView textView;
+
+        CustomViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.item_text);
+        }
+
+    }
+
+}
