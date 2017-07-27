394cbda375485c9c7507325cf0e748bc5ed3227e
Jeremy Kao <imsardine@gmail.com>
2017-07-27 15:44:28 +0800

    [android] ID of root view is dynamically replaced

--- android/usage/app/src/androidTest/java/com/example/usage/fragment/FragmentViaLayoutElementActivityTest.java
+++ android/usage/app/src/androidTest/java/com/example/usage/fragment/FragmentViaLayoutElementActivityTest.java
@@ -1,14 +1,21 @@
 package com.example.usage.fragment;
 
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
 
+import com.example.usage.R;
+
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 public class FragmentViaLayoutElementActivityTest {
 
@@ -16,8 +23,18 @@ public class FragmentViaLayoutElementActivityTest {
     public ActivityTestRule<FragmentViaLayoutElementActivity> activityTestRule = new ActivityTestRule<>(FragmentViaLayoutElementActivity.class);
 
     @Test
-    public void fragmentIsDisplayed() throws Exception {
-        onView(withText("Fragment A")).check(matches(isDisplayed()));
+    public void fragmentIsDisplayed() {
+        Matcher<View> button = withId(R.id.button);
+        onView(button).check(matches(allOf(withText("Fragment A"), isDisplayed())));
+
+    }
+
+    @Test
+    public void fragmentRootViewIdIsDynamicallyReplaced() {
+        Matcher<View> button = withId(R.id.button);
+
+        // The ID of the root view of the inflated layout comes from <fragment> element.
+        onView(withChild(button)).check(matches(withId(R.id.fragment_element)));
     }
 
 }
