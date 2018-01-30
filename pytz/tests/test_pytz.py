7b113bec868fc746bc3456776de3799293b948c7
Jeremy Kao <imsardine@gmail.com>
2018-01-30 23:03:34 +0800

    [pytz] Convert time zones

--- pytz/tests/test_pytz.py
+++ pytz/tests/test_pytz.py
@@ -15,3 +15,10 @@ def test_taipei_timezone():
     assert tz.utcoffset(REF_DT) == timedelta(hours=8) # UTC+8
     assert tz.dst(REF_DT) == timedelta(0) # DTS is not in effect
 
+def test_convert_timezone():
+    dt_utc = datetime(2018, 1, 1, 12, 30 ,45, tzinfo=pytz.utc)
+    tz_tpe = pytz.timezone('Asia/Taipei')
+    dt_tpe = dt_utc.astimezone(tz_tpe)
+
+    assert dt_tpe.tzinfo.tzname(None) == 'Asia/Taipei'
+    assert dt_tpe.replace(tzinfo=None) == datetime(2018, 1, 1, 20, 30, 45)
