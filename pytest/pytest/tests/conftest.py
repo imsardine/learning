7802329c856ca56661c7304da25c8c156e1f17e5
Jeremy Kao <imsardine@gmail.com>
2018-03-14 12:33:35 +0800

    [pytest] Add datafile.abspath()

--- pytest/pytest/tests/conftest.py
+++ pytest/pytest/tests/conftest.py
@@ -6,16 +6,19 @@ class DataFileHelper(object):
     def __init__(self, base_dir):
         self._base_dir = base_dir
 
-    def read(self, fn):
-        with open(path.join(self._base_dir, fn)) as f:
-            return f.read()
+    def abspath(self, fn):
+        return path.join(self._base_dir, fn)
 
-    def json(self, fn):
+    def read(self, fn, encoding=None):
+        with open(self.abspath(fn), 'rb') as f:
+            data = f.read()
+            return data.decode(encoding) if encoding else data
+
+    def json(self, fn, encoding='utf-8'):
         import json
-        return json.loads(self.read(fn))
+        return json.loads(self.read(fn, encoding))
 
 @pytest.fixture
 def datafile(request):
     base_dir = path.dirname(request.module.__file__)
     return DataFileHelper(base_dir)
-
