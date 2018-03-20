f1605766b6b4bb04c89e8faba916f1de59ad1695
Jeremy Kao <imsardine@gmail.com>
2018-03-20 23:03:38 +0800

    [python] Parse query string encoded with urlencode()

--- python/python/tests/test_url.py
+++ python/python/tests/test_url.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 import urllib, re
+import urlparse
 import pytest
 
-def test_urlencode__mapping():
-    data = {'key1': '123', 'key2': 'one two three'}
+def test_url_encode__mapping():
+    data = {'key': 'value', 'dt': '2018-03-20T17:00:00+08:00'}
 
     assert urllib.urlencode(data) in [
         # arbitrary order (non-random)
-        'key2=one+two+three&key1=123',
-        'key1=123&key2=one+two+three'
+        'dt=2018-03-20T17%3A00%3A00%2B08%3A00&key=value',
+        'key=value&dt=2018-03-20T17%3A00%3A00%2B08%3A00'
     ]
 
-def test_urlencode__unicode__raise_error():
+def test_url_decode__dict():
+    query_string = 'key=value&dt=2018-03-20T17%3A00%3A00%2B08%3A00'
+    params = urlparse.parse_qs(query_string)
+
+    assert params == {
+        'key': ['value'],
+        'dt': ['2018-03-20T17:00:00+08:00'] # list of values
+    }
+
+def test_url_encode__unicode__raise_error():
     data = {'unicode': u'中文'}
 
     message = "'ascii' codec can't encode characters in position 0-1:" \
@@ -19,7 +29,7 @@ def test_urlencode__unicode__raise_error():
     with pytest.raises(UnicodeEncodeError, match=re.escape(message)) as exc_info:
         urllib.urlencode(data)
 
-def test_urlencode__unicode_utf8encode():
+def test_url_encode__unicode_utf8encode():
     data = {'unicode-encoded': u'中文'.encode('utf-8')}
     assert urllib.urlencode(data) == 'unicode-encoded=%E4%B8%AD%E6%96%87'
 
