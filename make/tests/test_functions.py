8a29e18932957cd11f450ff0e18d90e4c84b2df7
Jeremy Kao <imsardine@gmail.com>
2018-06-20 23:38:59 +0800

    [make] User-defined functions

---
+++ make/tests/test_functions.py
@@ -0,0 +1,23 @@
+def test_user_defined_function(shell):
+    shell.src('Makefile', """
+    define hello
+    	@echo -n 'Hello,$(1)!'
+    endef
+
+    target:
+    	$(call hello, World)
+    """)
+
+    assert shell.run('make').out == 'Hello, World!'
+
+def test_user_defined_function__whitespaces_in_arguemnts(shell):
+    shell.src('Makefile', """
+    define echo
+    	@echo -n '[$(1)]' '[$(2)]' '[$(3)]'
+    endef
+
+    target:
+    	$(call echo, a, b,c)
+    """)
+
+    assert shell.run('make').out == '[ a] [ b] [c]'
