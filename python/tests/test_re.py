f46859b912a6956634a4f3fbd9e2aa63ebcb37da
Jeremy Kao <imsardine@gmail.com>
2018-06-27 21:25:20 +0800

    [python] Regex substitution (look ahead/behind)

--- python/tests/test_re.py
+++ python/tests/test_re.py
@@ -10,3 +10,11 @@ def test_named_group():
     assert match.group('number') == '8.7'
     assert match.group('unit') == 'K'
 
+def test_substitution__look_ahead_behind():
+    origin = 'Hello, World! Have a nice day.'
+    #           ^-- replace it
+
+    # re.sub(pattern, repl, string), replace match.group(0)
+    replaced = re.sub(r'(?<=Hello, )\w+(?=\! .+)', 'Regex', origin)
+    assert replaced == 'Hello, Regex! Have a nice day.'
+
