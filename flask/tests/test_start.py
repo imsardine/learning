6a3870af7e04cf6a96fe163adde99c78869f0030
Jeremy Kao <imsardine@gmail.com>
2018-06-28 10:45:17 +0800

    [flask] Improve test harness

--- flask/tests/test_start.py
+++ flask/tests/test_start.py
@@ -11,7 +11,26 @@ def test_hello_world(shell):
         return 'Hello, World!'
     """)
 
+    import flask
+    version = tuple(map(int, flask.__version__.split('.')))
+    if version[0] == 0: # 0.x
+        message = """
+        | * Serving Flask app "hello"
+        | * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+        """
+    else: # 1.x
+        message = """
+        | * Serving Flask app "hello.py"
+        | * Environment: production
+        |\x1b[31m   WARNING: Do not use the development server in a production environment.\x1b[0m
+        |\x1b[2m   Use a production WSGI server instead.\x1b[0m
+        | * Debug mode: off
+        | * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+        """
+
     with shell.spawn('FLASK_APP=hello.py flask run') as p:
+        p.expect_exact(message)
+
         resp = requests.get('http://localhost:5000')
         assert resp.text == 'Hello, World!'
 
