65f19d53c330d0da8e830f6122df4f3ce19f60a4
Jeremy Kao <imsardine@gmail.com>
2018-06-30 08:51:55 +0800

    [c] Dedent, to make code more clear

--- c/tests/test_start.py
+++ c/tests/test_start.py
@@ -2,12 +2,12 @@ import pytest
 
 def test_hello_world(cli):
     cli.src('hello.c', r"""
-        #include <stdio.h>
+    #include <stdio.h>
 
-        int main() {
-            printf("Hello, World!\n");
-            return 0;
-        }
+    int main() {
+        printf("Hello, World!\n");
+        return 0;
+    }
     """)
 
     cli.run('make hello')
@@ -17,8 +17,8 @@ def test_hello_world(cli):
 
 def test_printf__leading_zeros(cprog):
     out = cprog.run(r"""
-        int num = 8;
-        printf("The next one is #%03d", num);
+    int num = 8;
+    printf("The next one is #%03d", num);
     """)
 
     assert out == 'The next one is #008'
@@ -34,11 +34,11 @@ class _CProgram():
 
     def run(self, main_body):
         self._cli.src('main.c', """
-            #include <stdio.h>
+        #include <stdio.h>
 
-            int main() {
-                %s
-            }""" % main_body)
+        int main() {
+            %s
+        }""" % main_body)
 
         self._cli.run('make main')
         r = self._cli.run('./main')
