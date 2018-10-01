bf4238849f908f705b7c1055d2ead39e8cb95c77
Jeremy Kao <imsardine@gmail.com>
2018-10-01 15:37:02 +0800

    [requests] Query strings get constructed even there exists some params

--- requests/tests/test_request.py
+++ requests/tests/test_request.py
@@ -11,3 +11,8 @@ def test_query_string__none_values_excluded():
     resp = requests.get('http://httpbin/get', params=params)
     assert resp.url == 'http://httpbin/get?key=value&key-list=value1&key-list=value2'
     assert 'key-none' not in resp.url
+
+def test_query_string__url_with_query_string__append_properly():
+    params = {'key2': 'value2'}
+    resp = requests.get('http://httpbin/get?key=value', params=params)
+    assert resp.url == 'http://httpbin/get?key=value&key2=value2'
