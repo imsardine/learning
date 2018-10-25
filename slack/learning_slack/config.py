24629836d5278bb5233ea1d64f77df2a73b4c027
Jeremy Kao <imsardine@gmail.com>
2018-10-25 19:17:49 +0800

    Introduce interactive messages

--- slack/learning_slack/config.py
+++ slack/learning_slack/config.py
@@ -3,4 +3,6 @@ import os
 class Config():
 
     def __init__(self):
-        self.verification_token = os.environ['VERIFICATION_TOKEN']
+        self.slack_verification_token = os.environ['SLACK_VERIFICATION_TOKEN']
+        self.slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
+        self.slack_bot_token = os.environ['SLACK_BOT_TOKEN']
--- slack/learning_slack/hello.py
+++ slack/learning_slack/hello.py
@@ -1,16 +1,21 @@
+import logging
+import json
 from flask import Flask, request, abort, jsonify
 from .config import Config
 
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
 config = Config()
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET', 'POST'])
-def hello_():
+@app.route('/slack/slashcmd', methods=['GET', 'POST'])
+def slash_command_handler():
     # Slack may occasionally send a simple GET request to verify the URL and certificate.
     if request.method == 'GET' and request.args.get('ssl_check', None):
         return
 
-    if request.form['token'] != config.verification_token:
+    if request.form['token'] != config.slack_verification_token:
         abort(401) # Unauthorized
 
     user = request.form['user_name']
@@ -18,7 +23,39 @@ def hello_():
     args = request.form['text']
 
     text = 'Hello, %s! Hello, World!' % user
-    attached = {'text': 'And here is the original command:\n%s %s' % (cmd, args)}
+    attached = {
+        'callback_id': 'voila',
+        'text': 'And here is the original command:\n%s %s' % (cmd, args),
+        'actions': [
+            {
+                'name': 'type',
+                'text': 'What kind of question?',
+                'type': 'select',
+                'options': [
+                    {'text': 'Type 1', 'value': '1' + args },
+                    {'text': 'Type 2', 'value': '2' + args },
+                ],
+            },
+        ]
+    }
 
-    return jsonify({ 'text': text, 'attachments': [attached]})
+    # ephemeral, therefore no original_message in subsequent action payload
+    return jsonify({'text': text, 'attachments': [attached]})
+
+@app.route('/slack/action', methods=['POST'])
+def action_handler():
+    payload = json.loads(request.form['payload'])
+    user = payload['user']['name']
+    value = payload['actions'][0]['selected_options'][0]['value']
+    decision, question = value[0], value[1:]
+    logger.error('DECISION: %s' % decision)
+
+    channel = 'private-channel-%s' % decision
+
+    from slackclient import SlackClient
+    slack = SlackClient(config.slack_bot_token)
+    message = "<@%s> has a question, let's give him/her a favor ~\n\n%s" % (user, question)
+    slack.api_call('chat.postMessage', channel=channel, text=message)
+
+    return jsonify({'text': "The question has been redirected to support teams."})
 
