c30534ae95dd5d8ffbe449a842538fafd808c773
Jeremy Kao <imsardine@gmail.com>
2018-11-23 22:21:42 +0800

    [python] Improve tests relevant to None comparison

--- python/tests/test_none.py
+++ python/tests/test_none.py
@@ -1,6 +1,6 @@
 import types
 
-def test_none_singleton(py2):
+def test_singleton(py2):
     if py2:
         assert isinstance(None, types.NoneType)
     else:
@@ -12,20 +12,23 @@ class Negator(object):
     def __eq__(self, other):
         return not other # doesn't make sense
 
-def test_none_identity():
+    def __ne__(self, other): # requried for py2
+        return not self.__eq__(other)
+
+def test_comparison__use_identity():
     none = None
     thing = Negator()
 
     assert none is None # singleton
     assert thing is not None
 
-def test_none_equality():
+def test_comparison__donot_use_equality():
     none = None
     thing = Negator()
 
     assert none == None
     assert not (none != None)
 
-    assert thing == None # dependes on __eq__
-    assert thing != None
-
+    # weird? the result dependes on thing.__eq__()
+    assert thing == None
+    assert not (thing != None)
