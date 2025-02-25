89f461082df6eec27323b5c17b3e46808b9944f6
Jeremy Kao <jeremykao@kkcompany.com>
2025-02-26 07:52:12 +0800

    [typescript] transpilation for different targets

--- typescript/tests/test_start.py
+++ typescript/tests/test_start.py
@@ -17,3 +17,53 @@ def test_hello_world(workspace):
     # Run the generated JavaScript
     r = workspace.run('node hello.js')
     assert r.out == 'Hello TypeScript (TS)!'
+
+def test_transpilation(workspace):
+    workspace.src('hello.ts', '''
+    class Person {
+      private name: string;
+
+      constructor(name: string) {
+        this.name = name;
+      }
+
+      greet() {
+        return `Hello, ${this.name}!`;
+      }
+    }
+
+    console.log(new Person('World').greet());
+    ''')
+
+    # target: es5 (default)
+    r = workspace.run('tsc --strict hello.ts')
+    assert r.out == ''
+    assert workspace.read_txt('hello.js', 'utf-8') == lines('''
+    "use strict";
+    var Person = /** @class */ (function () {
+        function Person(name) {
+            this.name = name;
+        }
+        Person.prototype.greet = function () {
+            return "Hello, ".concat(this.name, "!");
+        };
+        return Person;
+    }());
+    console.log(new Person('World').greet());
+    ''')
+
+    # target: es6
+    r = workspace.run('tsc --strict --target es6 hello.ts')
+    assert r.out == ''
+    assert workspace.read_txt('hello.js', 'utf-8') == lines('''
+    "use strict";
+    class Person {
+        constructor(name) {
+            this.name = name;
+        }
+        greet() {
+            return `Hello, ${this.name}!`;
+        }
+    }
+    console.log(new Person('World').greet());
+    ''')
